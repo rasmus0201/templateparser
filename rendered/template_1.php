2dfd1d2341e56c91f6bebeaa67731b76a1eaacc9
rasmus0201 <xxr@live.dk>
2018-05-14 13:18:44 +0200

    Test Parser

---
+++ rendered/template_1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Site Title 2</title>
+    </head>
+    <body>
+        <h1>Page: Frontpage 2</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, amet.</p>
+        PHP Resultat: 20    </body>
+</html>
